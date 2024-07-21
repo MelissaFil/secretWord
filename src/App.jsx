72bfa242c071800e4c2ac0cb8b52b90aabdd0af0
Melissa <programadorimd@hotmail.com>
2024-07-21 11:01:57 -0300

    iniciando com primeira tela do jogo

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { StartScreen } from './components/StartScreen'
+
+function App() {
+
+  return (
+    <div className='App'> 
+
+      <h1>Secret Word</h1>
+      <StartScreen />
+
+    </div> 
+  )
+}
+
+export default App
---
+++ src/components/StartScreen.jsx
@@ -0,0 +1,9 @@
+import './StartScreen.css'
+export const StartScreen = () => {
+  return (
+    <div className="startScreen">
+        <p> Para começar, clique no botão abaixo</p>
+        <button>Começar jogo</button>
+    </div>
+  )
+}
