9c0545fdd53cb6b79e04cb4385f0c981f2a0d660
Melissa <programadorimd@hotmail.com>
2024-07-21 11:34:38 -0300

    criando referencias de páginas pra mudar o componente renderizado dependendo do stage que a aplicação esteja

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
+import { useState } from 'react'
 import './App.css'
 import { StartScreen } from './components/StartScreen'
 
+
+const stages = [
+  {id:1, name:'start'},
+  {id:2, name:'game'},
+  {id:3, name:'end'},
+]
+
 function App() {
+
+  const [currentStage, setCurrentStage] = useState(stages[0].name)
 
   return (
     <div className='App'> 
 
       <h1>Secret Word</h1>
-      <StartScreen />
+      {currentStage === 'start' && <StartScreen />}
 
     </div> 
   )
