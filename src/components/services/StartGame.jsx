4899edcacd6d241048d40599cb98b8ac36253e4f
Melissa <programadorimd@hotmail.com>
2024-07-22 13:26:25 -0300

    lower case na palavra pra n precisar colocar a primeira letra maiuscula pra acerta a palabra

--- src/components/services/StartGame.jsx
+++ src/components/services/StartGame.jsx
@@ -4,8 +4,9 @@ import { wordsList } from "../../data/word"
 const StartGame = () => {
   const data= Object.keys(wordsList)
   const category = data[Math.floor(Math.random() * data.length)]
-  const word = wordsList[category][Math.floor(Math.random() * wordsList[category].length)]
-
+  const word = wordsList[category][Math.floor(Math.random() * wordsList[category].length)].toLowerCase()
+  
+  console.log(category,word)
     return {category, word}
 }
 
